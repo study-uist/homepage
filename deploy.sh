bc9e4840a61905e6aa1b7864bdef5bcee750af9f
Akira Matsuda <akira.matsuda@me.com>
2018-01-25 22:19:51 +0900

    GAの追加

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,14 @@ set -e # halt script on error
 echo 'Jekyll build...'
 bundle exec jekyll build
 
+cp CNAME _site/
 cd _site
 
 # config
 
 # deploy
-git init
+# git init
 git add --all
 git commit -m "Deploy to GitHub Pages"
-git remote add origin "git@github.com:study-uist/study-uist.github.io.git"
+# git remote add origin "git@github.com:study-uist/study-uist.github.io.git"
 git push --force --quiet origin master
